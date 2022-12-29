96cb0abb76b2c9087d9a23ef8b00cfec7b751f35
Rushabh Gunjal <121039241+gunjalrushabh@users.noreply.github.com>
2022-12-30 02:13:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require("express");
 const app=express();
 const path=require("path");
 app.get("/",function(req,resp){
-    resp.send("shivprasaddddddd as a cco");
+    resp.send("Hello guys we will Rock...");
 });
 app.get("/home",function(req,resp){
     resp.sendFile(path.join(__dirname,"./public/index.html"));
